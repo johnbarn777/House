428ec8dd95bf38a67ffee3ba4ec569c89457db7f
johnbarn777 <johnbarn2409@gmail.com>
2025-05-09 15:29:41 -0700

    NOT WORKING: upgraded to react-native@0.79.1 & react@19.0.0

---
+++ ios/House/House-Bridging-Header.h
@@ -0,0 +1,6 @@
+// House-Bridging-Header.h
+#import <RCTAppDelegate.h>
+#import <React/RCTBridgeDelegate.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTDefines.h>
